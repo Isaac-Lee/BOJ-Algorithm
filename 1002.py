cba89b1ff4e80b7b7a49773e241bd10c41cba6b8
Isaac-Lee <yy0221ss@gmail.com>
2019-12-05 23:13:57 +0900

    pycharm commit

--- 1002.py
+++ 1002.py
@@ -1,8 +1,8 @@
 def length(x1, x2, y1, y2):
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2)**0.5
-
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 n = int(input())
+ans=[]
 for i in range(n):
     case = [int(k) for k in input().split()]
     x1 = case[0]
@@ -16,18 +16,21 @@ for i in range(n):
         r1 = r2
         r2 = c
     if (x1 == x2) and (y1 == y2) and (r1 == r2):
-        print(-1)
-    if (length(x1, x2, y1, y2) + r1) > r2:
-        if length(x1, x2, y1, y2) > r1 + r2:
-            print(0)
-        elif length(x1, x2, y1, y2) == r1 + r2:
-            print(1)
+        ans.append(-1)
+        pass
+    if (length(x1, x2, y1, y2))**2 > ((r2-r1)**2)**2:
+        if (length(x1, x2, y1, y2))**2 > ((r1 + r2)**2)**2:
+            ans.append(0)
+        elif (length(x1, x2, y1, y2))**2 == ((r1 + r2)**2)**2:
+            ans.append(1)
         else:
-            print(2)
-    elif (length(x1, x2, y1, y2) + r1) == r2:
-        print(1)
+            ans.append(2)
+    elif (length(x1, x2, y1, y2))**2 == ((r2-r1)**2)**2:
+        ans.append(1)
     else:
-        print(0)
+        ans.append(0)
 
+for n in ans:
+    print(n)
 
 # TODO 문제 틀렸음 다시 풀기

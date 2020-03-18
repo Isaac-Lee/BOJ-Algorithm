158cdeadf5844975ce10f4630704c02ffa6ab73b
isaac_lee <yy0221ss@gmail.com>
2020-03-18 14:35:06 +0900

    DP

---
+++ 9095.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def make123(n):
+    if n == 1 or n == 0:
+        memo[n] = 1
+        return 1
+    if memo[n] > 0:
+        return memo[n]
+
+    memo[n] += make123(n-1)
+    if n-3 >= 0:
+        memo[n] += make123(n-3)
+    if n-2 >= 0:
+        memo[n] += make123(n-2)
+    return memo[n]
+
+
+if __name__ == "__main__":
+    n = int(sys.stdin.readline())
+    for _ in range(n):
+        k = int(sys.stdin.readline())
+        memo = [0] * (k + 1)
+        print(make123(k))

f840651fb6a206115ded6856fdd36d8d74808c98
isaac_lee <yy0221ss@gmail.com>
2020-03-22 00:50:36 +0900

    DP

--- 1904.py
+++ 1904.py
@@ -1,14 +1,12 @@
 import sys
 
 def makeBy(n):
-    c1=1
-    c2=2
-    c3=0
     for i in range(3, n+1):
-        c3 = c1 + c2
-        c1, c2 = c2, c3
-    print(c3%15746)
+        memo[3] = memo[1] + memo[2]
+        memo[1], memo[2] = memo[2], memo[3]
+    print(memo[3]%15746)
 
 if __name__ == "__main__":
     n = int(sys.stdin.readline())
+    memo = [0,1,2,0]
     makeBy(n)

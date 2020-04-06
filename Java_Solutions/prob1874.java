e3a1b8ee3214e3037ff15d364d9c4af9b9caf8a5
isaac_lee <yy0221ss@gmail.com>
2020-04-06 23:40:19 +0900

    Java Solutions

---
+++ Java_Solutions/prob1874.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class prob1874 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(input.readLine());
+
+		int i, temp, max = 0;
+		Stack<Integer> s = new Stack<>();
+		StringBuilder answer = new StringBuilder();
+
+		while (n-- > 0) {
+			temp = Integer.parseInt(input.readLine());
+
+			if (temp > max) {
+				for (i = max+1; i < temp+1; i++) {
+					s.push(i);
+					answer.append("+");
+				}
+				max = temp;
+			} else if (s.peek() != temp) {
+				System.out.println("NO");
+				return;
+			}
+			s.pop();
+			answer.append("-");
+		}
+		for (i = 0; i < answer.length(); i++) {
+			System.out.println(answer.charAt(i));
+		}
+
+	}
+}

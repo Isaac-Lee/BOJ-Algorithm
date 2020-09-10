b4c013d07831fef8748e20a07189f5df0ceb8595
isaac_lee <yy0221ss@gmail.com>
2020-09-10 22:27:42 +0900

    commit via mac

--- Python_Solutions/11659.py
+++ Python_Solutions/11659.py
@@ -1,5 +1,21 @@
-n, t = map(int, input().split())
-arr = list(map(int, input().split()))
+import sys
+
+n, t = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+sum = [0 for i in range(n)]
+for i in range(n):
+    if i == 0:
+        sum[0] = arr[0]
+        continue
+    else:
+        sum[i] = sum[i-1] + arr[i]
 for _ in range(t):
-    i, k = map(int, input().split())
-    print(sum(arr[i-1:k]))
+    i, k = map(int, sys.stdin.readline().split())
+    if i > k:
+        temp = i
+        i = k
+        k = temp
+    if i == 1:
+        print(sum[k-1])
+    else:
+        print(sum[k-1]-sum[i-2])

0abfb2e759a3bef6761cbbfdab516dbd494dd709
Isaac-Lee <yy0221ss@gmail.com>
2023-11-09 21:54:06 +0900

    7576

--- Python_Solutions/7576.py
+++ Python_Solutions/7576.py
@@ -13,52 +13,44 @@ def is_inside(M, N, x, y):
 
 def bfs(starts, tomato, M, N):
     queue = deque()
-    visited = set()
-    days = 0
 
     for start in starts:
         queue.append(start)
 
     while queue:
-        next = len(queue)
-        for _ in range(next):
-            now = queue.popleft()
-            x, y = now
-            tomato[x][y] = 1
-            for direction in directions:
-                dx, dy = direction
-                new_x, new_y = x+dx, y+dy
-                if is_inside(M, N, new_x, new_y):
-                    if tomato[new_x][new_y] != 0:
-                        continue
-                    if (new_x, new_y) in visited:
-                        continue
+        x, y = queue.popleft()
+        for direction in directions:
+            dx, dy = direction
+            new_x, new_y = x + dx, y + dy
+            # 그래프 밖 좌표는 제외한다.
+            if is_inside(M, N, new_x, new_y):
+                # 이미 익힌 토마토와 토마토가 없는 칸은 건너뛴다.
+                if tomato[new_x][new_y] == 0:
+                    tomato[new_x][new_y] = tomato[x][y] + 1
                     queue.append((new_x, new_y))
-                    visited.add((new_x, new_y))
-        days += 1
-    return days-1
 
 
 def is_all_ripe(tomato):
+    max_days = 0
     for tomatoes in tomato:
-        if 0 in tomatoes:
-            return False
-    return True
+        for t in tomatoes:
+            if t == 0:
+                return 0
+            max_days = max(t, max_days)
+    return max_days
 
 
 if __name__ == '__main__':
-    M, N = [int(x) for x in input().split()]
-    tomato = []
+    M, N = map(int, input().split())
+    tomato = [list(map(int, input().split())) for _ in range(N)]
     starts = []
     for x in range(N):
-        tomatoes = []
-        for y, t in enumerate(input().split()):
-            tomatoes.append(int(t))
-            if t == '1':
+        for y in range(M):
+            if tomato[x][y] == 1:
                 starts.append((x, y))
-        tomato.append(tomatoes)
-    answer = bfs(starts, tomato, M, N)
-    if is_all_ripe(tomato):
-        print(answer)
+    bfs(starts, tomato, M, N)
+    answer = is_all_ripe(tomato)
+    if answer > 0:
+        print(answer-1)
     else:
         print(-1)

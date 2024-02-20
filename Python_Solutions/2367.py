e67fd458ea1927b74c492b5f891ced283ee9901b
이예성 <ysl@satreci.com>
2024-02-20 10:10:41 +0900

    not answer

--- Python_Solutions/2367.py
+++
@@ -1,8 +0,0 @@
-n, m = map(int, input().split())
-num = []
-for _ in range(n):
-    num.append(int(input()))
-for _ in range(m):
-    i, j = map(int, input().split())
-    l = num[i-1:j]
-    print(min(l), max(l))
